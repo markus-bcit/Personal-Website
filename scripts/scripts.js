7c4020eafd3d5fba36fe04c10baf18d6879dfa3b
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-11 14:59:56 -0800

    improves array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,24 @@
 
-const courselist =  [{ code: ["ACIT 1620", "ORGB 1110", "ACIT 1515"],  name: ["Web Fundamental Technologies", "Orginizational Behaviour", "Scripting for IT"]}]
+const courselist = [{ code: "ACIT 1620", name: "Web Fundamental Technologies"},
+                    {code: "ORGB 1110", name: "Orginizational Behaviour"},
+                    {code: "ACIT 1515", name: "Scripting for IT"}]
 
 let input;
 
 do {
-    input = prompt("Enter a 4 digit class number:");
+input = prompt("Enter a 4 digit class number:");
 
-    if (input !== '1620' && input !== '1110' && input !== '1515'){
-        console.log("Not a class number");
-        courselist.push({code: input, name})
-    }
+if (input !== '1620' && input !== '1110' && input !== '1515'){
+console.log("Not a class number, input added to array");
+courselist.push({code: input, name})
+}
 } while (input !== '1620' && input !== '1110' && input !== '1515');
 
-for (let x = 0; x < courselist[0]['code'].length; x++) 
+for (let x = 0; x < courselist.length; x++) 
 {
-    if (courselist[0]['code'][x].includes(input)) {
-        console.log(`Yes I am taking the course: ${courselist[0]['code'][x]} - ${courselist[0]['name'][x]}`)
-    }
-        
+if (courselist[x]['code'].includes(input)) {
+console.log(`Yes I am taking the course: ${courselist[x]['code']} - ${courselist[x]['name']}`)
+}
+
 }
 console.log(courselist)
